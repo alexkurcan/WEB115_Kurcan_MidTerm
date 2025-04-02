4f3886c131533863001c517105a72b726261fa78
Alex Kurcan <akurcan@my.waketech.edu>
2025-04-02 13:18:31 -0400

    adding files for website

---
+++ codingExam.js
@@ -0,0 +1,8 @@
+const button = document.getElementById("Button1");
+button.addEventListener('click', function(){
+    let loanAmount = prompt("Please enter your loan amount (without commas)")
+    let intialDownPayment = prompt("Please enter the amount of your down payment")
+    let loanTermYears = prompt("Please enter the loan term in years (either 30 or 15)")
+
+    
+})
